4107f628d086c8c79af1642a7cf95694de854fe9
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-27 16:46:41 -0400

    MVP met

--- index.js
+++ index.js
@@ -48,7 +48,13 @@ server.put('/api/cohorts/:id', (req,res) => {
 })
 
         //get students by cohort
-
+server.get('/api/cohorts/:id/students', (req,res) => {
+    const id = req.params.id
+    db('students')
+       .where({ cohort_id: id })
+       .then(students => res.status(200).json(students))
+       .catch(err => res.status(500).json(err))
+})
 
 
 
