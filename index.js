bd99b894d52cbe35ffa88f92364f73b9760a94ae
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-27 16:55:05 -0400

    separated into router

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
 const express = require('express');
-const knex = require('knex');
-const knexConfig = require('./knexfile.js')
-
-
-const db = knex(knexConfig.development);
 const server = express();
+
+const cohorts = require('./routers/cohorts-router');
+
 server.use(express.json());
-
-        //baseline cohort routes
-server.get('/api/cohorts', (req,res) => {
-    db('cohorts')
-        .then(chs => res.status(200).json(chs))
-        .catch(err => res.status(500).json(err))     
-})
-server.post('/api/cohorts', (req,res) => {
-    db('cohorts')
-        .insert(req.body)
-        .then(ids => {
-            const id = ids[0]
-            db('cohorts')
-                .where({ id: id })
-                .first()
-                .then(inserted => res.status(201).json(inserted))
-                .catch(err => res.status(404).json(err))
-        })
-        .catch(err => res.status(500).json(err))
-        
-})
-server.get('/api/cohorts/:id', (req,res) => {
-    db('cohorts')
-        .where({ id: req.params.id })
-        .then(cohort => res.status(200).json(cohort))
-        .catch(err => res.status(500).json(err))
-})
-server.put('/api/cohorts/:id', (req,res) => {
-    db('cohorts')
-        .where({id: req.params.id})
-        .update(req.body)
-        .then(count => {
-            if(count > 0){
-                res.status(204).json('successfully updated')
-            }else{
-                res.status(404).json('cohort not found')
-            }
-        })
-        .catch(err => res.status(500).json(err))
-})
-
-        //get students by cohort
-server.get('/api/cohorts/:id/students', (req,res) => {
-    const id = req.params.id
-    db('students')
-       .where({ cohort_id: id })
-       .then(students => res.status(200).json(students))
-       .catch(err => res.status(500).json(err))
-})
+server.use('/api/cohorts', cohorts);
 
 
 
---
+++ routers/cohorts-router.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+const knex = require('knex');
+const knexConfig = require('../knexfile')
+const db = knex(knexConfig.development);
+
+router.get('/', (req,res) => {
+    db('cohorts')
+        .then(chs => res.status(200).json(chs))
+        .catch(err => res.status(500).json(err))     
+})
+router.post('/', (req,res) => {
+    db('cohorts')
+        .insert(req.body)
+        .then(ids => {
+            const id = ids[0]
+            db('cohorts')
+                .where({ id: id })
+                .first()
+                .then(inserted => res.status(201).json(inserted))
+                .catch(err => res.status(404).json(err))
+        })
+        .catch(err => res.status(500).json(err))
+        
+})
+router.get('/:id', (req,res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .then(cohort => res.status(200).json(cohort))
+        .catch(err => res.status(500).json(err))
+})
+router.put('/:id', (req,res) => {
+    db('cohorts')
+        .where({id: req.params.id})
+        .update(req.body)
+        .then(count => {
+            if(count > 0){
+                res.status(204).json('successfully updated')
+            }else{
+                res.status(404).json('cohort not found')
+            }
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+        //get students by cohort
+router.get('/:id/students', (req,res) => {
+    const id = req.params.id
+    db('students')
+       .where({ cohort_id: id })
+       .then(students => res.status(200).json(students))
+       .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
